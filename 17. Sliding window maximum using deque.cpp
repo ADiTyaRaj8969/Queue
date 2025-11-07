67e5fd75a62149dd3344a00c448e0c155543a9ce
Aditya <147165909+ADiTyaRaj8969@users.noreply.github.com>
2025-11-08 01:04:27 +0530

    Add files via upload

---
+++ 17. Sliding window maximum using deque.cpp
@@ -0,0 +1,38 @@
+/*
+ Task: Sliding window maximum using deque (O(n))
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> slidingMax(vector<int>& a, int k)
+{
+    deque<int> dq;
+    vector<int> res;
+
+    for (int i = 0; i < static_cast<int>(a.size()); ++i)
+    {
+        while (!dq.empty() && dq.front() <= i - k) dq.pop_front();
+
+        while (!dq.empty() && a[dq.back()] <= a[i]) dq.pop_back();
+
+        dq.push_back(i);
+
+        if (i >= k - 1)
+            res.push_back(a[dq.front()]);
+    }
+
+    return res;
+}
+
+int main()
+{
+    vector<int> a = {1, 3, -1, -3, 5, 3, 6, 7};
+    int k = 3;
+    for (auto x : slidingMax(a, k)) cout << x << " ";
+    cout << "\n";
+    return 0;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(k)
